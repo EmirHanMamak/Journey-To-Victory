964fb83806562e55c9d57f2c09cadd560baa29f9
Emir Han Mamak <emirhanmamak6@gmail.com>
2023-08-16 01:17:02 +0300

    GameMusic  Added, Distance Counter Added GUI and Script

--- Assets/Scripts/Collectables/CommonScripts/CollectableControl.cs
+++ Assets/Scripts/Collectables/CommonScripts/CollectableControl.cs
@@ -7,12 +7,31 @@ using UnityEngine.UI;
 public class CollectableControl : MonoBehaviour
 {
     public TextMeshProUGUI coinCountText;
+    public TextMeshProUGUI distanceCountText;
     public static int coinCount = 0;
+    public static int distanceCount = 0;
+    public bool addingDistance = false;
+    Mover mover;
+
 
     void Update()
     {
         coinCountText.text = coinCount.ToString();
-        if(GameConditions.gameEnded == false) return;
+        distanceCountText.text = distanceCount.ToString();
+        if(!GameConditions.gameStarted) return;
+        if (addingDistance == false)
+        {
+            addingDistance = true;
+            StartCoroutine(AddingDistance());
+        }
+        if (GameConditions.gameEnded == false) return;
         coinCount = 0;
+        distanceCount = 0;
+    }
+    IEnumerator AddingDistance()
+    {
+        distanceCount++;
+        yield return new WaitForSeconds(0.25f);
+        addingDistance = false;
     }
 }
